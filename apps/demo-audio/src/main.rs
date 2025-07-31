334d9ca396eee643ae0c5866a2fd5fb99df471e9
Kurylko Maxim <kurylko.m@ajax.systems>
2025-07-31 18:12:19 +0300

    More ECS

--- apps/demo-audio/src/main.rs
+++ apps/demo-audio/src/main.rs
@@ -1,19 +1,26 @@
 use common::logging::CommonLogger;
 use common::resources::YARCResourceManagerIO;
+use evenio::component::Component;
+use evenio::event::{Receiver, Sender};
+use evenio::fetch::Single;
+use evenio::world::World;
+use glam::*;
 use log::info;
 use std::sync::Arc;
-use evenio::world::World;
 use yage2_audio::backend::PlayerBackendConfig;
 use yage2_audio::entities::bus::Bus;
-use yage2_audio::entities::effects::bypass::BypassEffect;
 use yage2_audio::entities::effects::soft_clip::SoftClipEffect;
+use yage2_audio::entities::events::{AudioEvent, AudioEventTargetId, AudioEventType};
 use yage2_audio::entities::sinks::InterleavedSink;
-use yage2_audio::entities::sources::actor::ActorsSource;
+use yage2_audio::entities::sources::actor::{
+    ActorID, ActorsSource, ActorsSourceEvent, DistanceGainFunction, DistanceLPFFunction,
+};
 use yage2_audio::player::{Player, PlayerConfig, ProfileFrame};
 use yage2_audio::resources::{
     FLACResourceFactory, MIDIResourceFactory, OGGResourceFactory, WAVResourceFactory,
 };
-use yage2_core::resources::{ResourceManager, ResourceManagerConfig, ResourceType};
+use yage2_core::ecs::{run_loop, StopEventLoop, Tick};
+use yage2_core::resources::{Resource, ResourceManager, ResourceManagerConfig, ResourceType};
 
 #[cfg(target_os = "linux")]
 // Alsa backend works A LOT better with 44,100 Hz sample rate
@@ -21,7 +28,9 @@ const SAMPLE_RATE: usize = 44100;
 #[cfg(not(target_os = "linux"))]
 const SAMPLE_RATE: usize = 48000;
 
-fn profile_player(frame: &ProfileFrame) {
+const REFRESH_RATE: f32 = 60.0; // Main loop refresh rate in ticks per second
+
+fn profile_player(frame: &yage2_audio::player::ProfileFrame) {
     // Number of samples that actually processed by one render call
     // (assuming that no underruns happens).
     let av_actual_samples = frame.sample_rate as f32 / frame.render_tps_av as f32;
@@ -49,53 +58,141 @@ fn profile_player(frame: &ProfileFrame) {
     );
 }
 
+fn profile_main_loop(profile_frame: &yage2_core::ecs::ProfileFrame) {
+    let allowed_time = 1000.0 / REFRESH_RATE;
+
+    info!(
+        "Main loop: {:.1}tps ({:.1}%)",
+        profile_frame.tick_tps_av,
+        profile_frame.tick_time_av / allowed_time * 1000.0
+    );
+}
+
+#[derive(Component)]
+struct GameController {
+    actors_source_target: AudioEventTargetId,
+    resource_manager: ResourceManager,
+    counter: usize,
+}
+
+impl Drop for GameController {
+    fn drop(&mut self) {
+        info!("GameController dropped");
+        self.resource_manager.finalize_all(ResourceType::AudioWAV);
+    }
+}
+
+impl GameController {
+    fn attach_to_ecs(self, world: &mut World) {
+        let entity = world.spawn();
+        world.insert(entity, self);
+    }
+
+    pub fn setup_resource_manager() -> ResourceManager {
+        // Setup resource manager
+        let resource_manager = ResourceManager::new(ResourceManagerConfig {
+            backend: Box::new(YARCResourceManagerIO::new("demo_audio.yarc".to_string())),
+        });
+        resource_manager.register_factory(
+            ResourceType::AudioWAV,
+            Arc::new(WAVResourceFactory::new(SAMPLE_RATE)),
+        );
+        resource_manager.register_factory(
+            ResourceType::AudioOGG,
+            Arc::new(OGGResourceFactory::new(SAMPLE_RATE)),
+        );
+        resource_manager.register_factory(
+            ResourceType::AudioFLAC,
+            Arc::new(FLACResourceFactory::new(SAMPLE_RATE)),
+        );
+        resource_manager.register_factory(
+            ResourceType::AudioMIDI,
+            Arc::new(MIDIResourceFactory::new()),
+        );
+        resource_manager.poll_io().unwrap();
+        resource_manager
+    }
+
+    fn setup_audio_pipeline(world: &mut World) -> AudioEventTargetId {
+        // Setup Audio Pipeline
+        fn leak<T>(value: T) -> &'static T {
+            Box::leak(Box::new(value))
+        }
+        let actors_source = leak(ActorsSource::new(
+            DistanceGainFunction::Constant(1.0),
+            DistanceLPFFunction::Constant(0.0),
+        ));
+        let clipper = leak(SoftClipEffect::new(0.5, 0.1));
+        let bus = Bus::new(clipper, actors_source, None, None);
+
+        // Start Audio Player (output)
+        let sink = InterleavedSink::new(bus, SAMPLE_RATE);
+        let config = PlayerConfig {
+            backend_config: PlayerBackendConfig {},
+            profiler: Some(profile_player),
+            sample_rate: SAMPLE_RATE,
+        };
+        let player = Player::new(config, sink).unwrap();
+
+        // Allow player to receive events from the ECS
+        // (moved to the ECS storage)
+        player.attach_to_ecs(world);
+
+        actors_source.get_id()
+    }
+
+    pub fn setup(world: &mut World) {
+        // Move controller to the ECS
+        GameController {
+            actors_source_target: Self::setup_audio_pipeline(world),
+            resource_manager: Self::setup_resource_manager(),
+            counter: 0,
+        }
+        .attach_to_ecs(world);
+    }
+
+    pub fn add_audio_actor(&self, pos: Vec3, gain: f32, clip: Resource) -> (AudioEvent, ActorID) {
+        let actor_id = ActorID::new();
+        (
+            AudioEvent::new(
+                self.actors_source_target,
+                AudioEventType::Actors(ActorsSourceEvent::AddActor {
+                    id: Some(actor_id.clone()),
+                    pos,
+                    gain,
+                    clip, // This should be set to a valid audio clip
+                }),
+            ),
+            actor_id,
+        )
+    }
+}
+
+fn timeout_handler(t: Receiver<Tick>, mut stopper: Sender<StopEventLoop>) {
+    if t.event.time > 10.0 {
+        // Stop the event loop after 10 seconds
+        stopper.send(StopEventLoop);
+    }
+}
+
+fn player_handler(_: Receiver<Tick>, mut gc: Single<&mut GameController>, mut sender: Sender<AudioEvent>) {
+    if gc.counter == 0 {
+        let clip = gc.resource_manager.get_resource("loop").unwrap();
+        sender.send(gc.add_audio_actor(Vec3::ZERO, 0.7, clip).0);
+        gc.counter += 1;
+    }
+}
+
 fn main() {
     // Initialize logging
     log::set_logger(&CommonLogger).unwrap();
-    log::set_max_level(log::LevelFilter::Info);
-
-    // Setup resource manager
-    let resource_manager = Arc::new(ResourceManager::new(ResourceManagerConfig {
-        backend: Box::new(YARCResourceManagerIO::new("demo_audio.yarc".to_string())),
-    }));
-    resource_manager.register_factory(
-        ResourceType::AudioWAV,
-        Arc::new(WAVResourceFactory::new(SAMPLE_RATE)),
-    );
-    resource_manager.register_factory(
-        ResourceType::AudioOGG,
-        Arc::new(OGGResourceFactory::new(SAMPLE_RATE)),
-    );
-    resource_manager.register_factory(
-        ResourceType::AudioFLAC,
-        Arc::new(FLACResourceFactory::new(SAMPLE_RATE)),
-    );
-    resource_manager.register_factory(
-        ResourceType::AudioMIDI,
-        Arc::new(MIDIResourceFactory::new()),
-    );
-    resource_manager.poll_io().unwrap();
+    log::set_max_level(log::LevelFilter::Debug);
 
     let mut world = World::new();
+    let _ = GameController::setup(&mut world);
 
-    // Setup Audio Pipeline
-    fn leak<T>(value: T) -> &'static T {
-        Box::leak(Box::new(value))
-    }
-    let actors_source = leak(ActorsSource::new());
-    let clipper = leak(BypassEffect::new());
-    let bus = Bus::new(clipper, actors_source, None, None);
+    world.add_handler(timeout_handler);
+    world.add_handler(player_handler);
 
-    // Start Audio Player (output)
-    let sink = InterleavedSink::new(bus, SAMPLE_RATE);
-    let config = PlayerConfig {
-        backend_config: PlayerBackendConfig {},
-        profiler: Some(profile_player),
-        sample_rate: SAMPLE_RATE,
-    };
-    let player = Player::new(config, sink).unwrap();
-    // Allow player to receive events from the ECS
-    player.attach_to_ecs(&mut world);
-
-    let entity = world.spawn();
+    run_loop(&mut world, REFRESH_RATE, Some(profile_main_loop));
 }
--- crates/yage2-core/src/ecs.rs
+++ crates/yage2-core/src/ecs.rs
@@ -1,13 +1,164 @@
+use crate::profile::{PeriodProfiler, TickProfiler};
 use evenio::component::Component;
-use evenio::event::GlobalEvent;
+use evenio::event::{GlobalEvent, Receiver};
+use evenio::fetch::Single;
+use evenio::world::World;
 use glam::Vec3;
+use log::{info, warn};
 
 #[derive(GlobalEvent)]
-pub struct Tick;
+pub struct Tick {
+    pub delta: f32,
+    pub time: f32,
+}
+
+#[derive(GlobalEvent)]
+pub struct StopEventLoop;
+
 #[derive(Component, Debug)]
 pub struct Position(Vec3);
+
 #[derive(Component, Debug)]
 pub struct Head {
     pub direction: Vec3,
     pub position: Vec3,
+}
+
+#[derive(Component, Debug)]
+struct PrivateData {
+    stopped: bool,
+}
+
+fn stop_event_loop_handler(_: Receiver<StopEventLoop>, mut d: Single<&mut PrivateData>) {
+    d.stopped = true;
+}
+
+pub struct ProfileFrame {
+    pub tick_time_min: f32,
+    pub tick_time_max: f32,
+    pub tick_time_av: f32,
+    pub tick_tps_min: f32,
+    pub tick_tps_max: f32,
+    pub tick_tps_av: f32,
+}
+
+struct Profiler<F>
+where
+    F: FnMut(&ProfileFrame) + Send + Sync + 'static,
+{
+    handler: Option<F>,
+    tick_profiler: TickProfiler,
+    period_profiler: PeriodProfiler,
+    last_profile_time: std::time::Instant,
+}
+
+impl<F> Profiler<F>
+where
+    F: FnMut(&ProfileFrame) + Send + Sync + 'static,
+{
+    pub fn new(tps: f32, handler: Option<F>) -> Self {
+        Profiler {
+            handler,
+            tick_profiler: TickProfiler::new(tps),
+            period_profiler: PeriodProfiler::new(1.0),
+            last_profile_time: std::time::Instant::now(),
+        }
+    }
+
+    pub fn tick_start(&self) {
+        if let Some(_) = &self.handler {
+            self.period_profiler.start();
+        }
+    }
+
+    pub fn tick_end(&mut self) {
+        if let Some(_) = &self.handler {
+            self.period_profiler.end();
+        }
+    }
+
+    pub fn profile(&mut self) {
+        if let Some(handler) = &mut self.handler {
+            self.tick_profiler.tick(1);
+
+            // Check if one second has passed since the last profile
+            if self.last_profile_time.elapsed().as_secs_f32() >= 1.0 {
+                self.last_profile_time = std::time::Instant::now();
+                self.tick_profiler.update();
+
+                let (tick_time_min, tick_time_av, tick_time_max) = self.period_profiler.get_stat();
+                let (tick_tps_min, tick_tps_av, tick_tps_max) = self.tick_profiler.get_stat();
+
+                let profile_frame = ProfileFrame {
+                    tick_time_min,
+                    tick_time_max,
+                    tick_time_av,
+                    tick_tps_min,
+                    tick_tps_max,
+                    tick_tps_av,
+                };
+
+                // Call the handler with the profile frame
+                handler(&profile_frame);
+            }
+        }
+    }
+}
+
+pub fn run_loop<F>(world: &mut World, tps: f32, profiler: Option<F>)
+where
+    F: FnMut(&ProfileFrame) + Send + Sync + 'static,
+{
+    world.add_handler(stop_event_loop_handler);
+
+    // Insert a private data component to track the stopped state
+    let entity = world.spawn();
+    world.insert(entity, PrivateData { stopped: false });
+
+    let mut prev_tick = std::time::Instant::now();
+    let loop_start = std::time::Instant::now();
+    let mut profiler = Profiler::new(tps, profiler);
+
+    loop {
+        profiler.profile();
+
+        // Check if the event loop should stop
+        if let Some(private_data) = world.get::<PrivateData>(entity) {
+            if private_data.stopped {
+                info!("Stopping event loop");
+                break;
+            }
+        }
+
+        // Remember the start time to keep the loop running at a fixed rate
+        let start = std::time::Instant::now();
+
+        // Calculate the delta time
+        let delta = start.duration_since(prev_tick).as_secs_f32();
+        let total_time = start.duration_since(loop_start).as_secs_f32();
+
+        // Dispatch the Tick event
+        profiler.tick_start();
+        world.send(Tick {
+            delta,
+            time: total_time,
+        });
+        profiler.tick_end();
+
+        // Update the previous tick time
+        prev_tick = start;
+
+        // Sleep to maintain the target ticks per second
+        let target_duration = std::time::Duration::from_secs_f32(1.0 / tps);
+        let elapsed = start.duration_since(prev_tick);
+        if elapsed < target_duration {
+            let sleep_duration = target_duration - elapsed;
+            std::thread::sleep(sleep_duration);
+        } else {
+            warn!(
+                "Tick took longer than expected: {:.3} seconds",
+                elapsed.as_secs_f32()
+            );
+        }
+    }
 }
