da4123fe72b3a13506d21c8766fbf4a8ff00e17b
Maxim Kurylko <kurylko.m@ajax.systems>
2025-08-09 17:40:23 +0300

    renderer factories

--- apps/demo-graphics/src/main.rs
+++ apps/demo-graphics/src/main.rs
@@ -1,11 +1,13 @@
-use common::logging::CommonLogger;
 use common::assets::YARCReader;
+use common::logging::CommonLogger;
 use evenio::component::Component;
 use evenio::event::{Receiver, Sender};
 use evenio::world::World;
 use log::info;
 use std::time::Duration;
+use yage2_core::assets::factory::FactoryBinding;
 use yage2_core::assets::hub::AssetHub;
+use yage2_core::assets::AssetType;
 use yage2_core::ecs::{run_loop, MainLoopProfileFrame, StopEventLoop, Tick};
 use yage2_graphics::input::{InputEvent, KeyCode};
 use yage2_graphics::renderer::{Renderer, RendererBackendConfig, RendererProfileFrame};
@@ -22,7 +24,26 @@ impl GameController {
         world.insert(entity, self);
     }
 
-    pub fn setup_graphics(world: &mut World) {
+    pub fn setup_asset_hub(world: &mut World) -> (FactoryBinding, FactoryBinding) {
+        let reader = YARCReader::new("demo_graphics.yarc".to_string());
+        let mut manager = AssetHub::new(reader).unwrap();
+        
+        // Unlike other factories, shader and texture assets are 
+        // managed directly by the renderer, instead of processing assets
+        // in the main loop (via ECS).
+        let shader_binding = manager.create_factory_biding(AssetType::ShaderSPIRV);
+        let texture_binding = manager.create_factory_biding(AssetType::ImagePNG);
+
+        manager.attach_to_ecs(world);
+
+        (shader_binding, texture_binding)
+    }
+
+    pub fn setup_graphics(
+        world: &mut World,
+        shader_binding: FactoryBinding,
+        texture_binding: FactoryBinding,
+    ) {
         let view_config = ViewConfig {
             platform_specific: PlatformSpecificViewConfig {},
             title: "Hello world".to_string(),
@@ -30,35 +51,26 @@ impl GameController {
             height: 600,
         };
         let backend_config = RendererBackendConfig {
-            fps: REFRESH_RATE as usize,
-            vsync: true,
+            shader_factory_binding: Some(shader_binding),
+            texture_factory_binding: Some(texture_binding),
         };
+        
         let renderer = Renderer::new(view_config, backend_config, true).unwrap();
         renderer.attach_to_ecs(world);
     }
 
-    pub fn setup_asset_hub(world: &mut World) {
-        let reader = YARCReader::new("demo_graphics.yarc".to_string());
-        let mut manager = AssetHub::new(reader).unwrap();
-        
-        // TODO: Setup factories
-        
-        manager.attach_to_ecs(world);
-    }
-
     pub fn setup(world: &mut World) {
-        Self::setup_graphics(world);
+        let (shader_binding, texture_binding) = Self::setup_asset_hub(world);
+        Self::setup_graphics(world, shader_binding, texture_binding);
         GameController {}.attach_to_ecs(world);
     }
 }
 
 fn main_loop_profile_handler(r: Receiver<MainLoopProfileFrame>) {
-    let allowed_time = 1000.0 / REFRESH_RATE;
-
     info!(
         "Main loop: {:.1}tps ({:.1}%)",
         r.event.tick_tps.average(),
-        r.event.tick_time.average() / allowed_time * 1000.0
+        r.event.average_load * 100.0
     );
 }
 
