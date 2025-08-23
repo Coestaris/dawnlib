c2fbe4b031bdf6d7a224e57fb3492dff870be49e
Kurylko Maxim <vk_vm@ukr.net>
2025-08-23 22:40:54 +0300

    start of new yarc format

--- crates/dac/src/lib.rs
+++ crates/dac/src/lib.rs
@@ -1,51 +1,10 @@
-use dawn_assets::ir::IRAsset;
-use dawn_assets::AssetHeader;
 use serde::{Deserialize, Serialize};
 use std::fmt::Display;
 
+mod layout;
 pub mod manifest;
 pub mod reader;
 pub mod writer;
-mod layout;
-
-#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
-pub enum Compression {
-    None,
-    Gzip,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
-pub enum ReadMode {
-    Flat,
-    Recursive,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
-pub enum ChecksumAlgorithm {
-    Md5,
-    Blake3,
-}
-
-impl Display for ChecksumAlgorithm {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Md5 => write!(f, "MD5"),
-            Self::Blake3 => write!(f, "Blake3"),
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct WriteOptions {
-    pub compression: Compression,
-    pub read_mode: ReadMode,
-    pub checksum_algorithm: ChecksumAlgorithm,
-
-    pub author: Option<String>,
-    pub description: Option<String>,
-    pub version: Option<String>,
-    pub license: Option<String>,
-}
 
 #[cfg(any())]
 pub mod serialize_backend {
@@ -65,25 +24,16 @@ pub mod serialize_backend {
 
 #[cfg(all())]
 pub mod serialize_backend {
+    use bincode;
     use serde::{Deserialize, Serialize};
+
     pub fn serialize<T: Serialize>(object: &T) -> Result<Vec<u8>, String> {
-        rmp_serde::to_vec::<T>(object).map_err(|e| format!("Failed to serialize AssetRaw: {}", e))
+        bincode::encode_to_vec(object, bincode::config::standard()).map_err(|e| e.to_string())
     }
 
     pub fn deserialize<'a, T: Deserialize<'a>>(bytes: &'a [u8]) -> Result<T, String> {
-        rmp_serde::from_slice::<'a, T>(bytes)
-            .map_err(|e| format!("Failed to deserialize AssetRaw: {}", e))
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub(crate) struct PackedAsset {
-    pub header: AssetHeader,
-    pub ir: IRAsset,
-}
-
-impl PackedAsset {
-    pub fn new(header: AssetHeader, ir: IRAsset) -> Self {
-        Self { header, ir }
+        bincode::decode_from_slice(bytes, bincode::config::standard())
+            .map(|(obj, _)| obj)
+            .map_err(|e| e.to_string())
     }
 }
